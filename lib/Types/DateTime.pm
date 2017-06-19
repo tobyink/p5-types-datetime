212d003a78094d1b8db4ffa8da5c200e0eb6b863
Toby Inkster <mail@tobyinkster.co.uk>
2017-06-19 17:14:17 +0100

    bump version; changelog

--- lib/Types/DateTime.pm
+++ lib/Types/DateTime.pm
@@ -5,7 +5,7 @@ use warnings;
 package Types::DateTime;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use DateTime;
 use DateTime::Duration;
@@ -357,7 +357,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2014 by Toby Inkster.
+This software is copyright (c) 2014, 2017 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
